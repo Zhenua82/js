179b46a0c078cd8a66fa8570da446e3a4f8f07f5
Zhenua82 <zzhmenka@yandex.ru>
2025-07-05 19:31:06 +0300

    chern

--- chern.js
+++ chern.js
@@ -6,7 +6,10 @@ for (let i = 0; i < 5; i++){
     }, 3000)
 }
 
+let a = [1, 2, 3, 4, 5];
 console.log(3 && 2 && 7)
 console.log(4 && 6 || 0)
 console.log(6 && 4 || 0)
 console.log(0 || 4 && 8)
+console.table(a)
+console.error('Это ошибка')
